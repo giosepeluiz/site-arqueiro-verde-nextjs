1ae87f86f39353c86046094f508bd4fe2c799694
Giosepe Luiz <giosepe_luiz3@live.com>
2021-08-03 00:07:47 -0300

    WIP

--- components/ChannelStatus/index.jsx
+++ components/ChannelStatus/index.jsx
@@ -2,7 +2,10 @@ import ytch from 'yt-channel-info';
 
 function youtube(id) {
     const channelId = id;
-    ytch.getChannelInfo(channelId).then((response) => (response)).catch((err) => (err))
+    ytch
+      .getChannelInfo(channelId)
+      .then((response) => response)
+      .catch((err) => err);
 }
 
 
