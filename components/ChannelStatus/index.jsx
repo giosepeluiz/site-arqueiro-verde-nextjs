d03781e01aafd5b3ea63a3516e27bc0ab413f634
Giosepe Luiz <giosepe_luiz3@live.com>
2021-08-03 00:18:44 -0300

    WIP

--- components/ChannelStatus/index.jsx
+++ components/ChannelStatus/index.jsx
@@ -10,23 +10,26 @@ function youtube(id) {
 
 
 export default function ChannelStatus(){
+  /* eslint-disable-next-line */
+  console.log(youtube("UCjzB67LSKFcDdkEcYX6Q7rg"));
 
-    console.log(youtube("UCjzB67LSKFcDdkEcYX6Q7rg"));
-
-    return(
-        <section className="channel-status">
-            <div className="status-one">
-                <span className="status-title">900</span><br />
-                <span className="status-subtitle">inscritos</span>
-            </div>
-            <div className="status-two">
-                <span className="status-title">1180</span><br />
-                <span className="status-subtitle">vídeos</span>
-            </div>
-            <div className="status-three">
-                <span className="status-title">3000</span><br />
-                <span className="status-subtitle">visualizações</span>
-            </div>
-        </section>
-    )    
+  return (
+    <section className="channel-status">
+      <div className="status-one">
+        <span className="status-title">900</span>
+        <br />
+        <span className="status-subtitle">inscritos</span>
+      </div>
+      <div className="status-two">
+        <span className="status-title">1180</span>
+        <br />
+        <span className="status-subtitle">vídeos</span>
+      </div>
+      <div className="status-three">
+        <span className="status-title">3000</span>
+        <br />
+        <span className="status-subtitle">visualizações</span>
+      </div>
+    </section>
+  );
 }
