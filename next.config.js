3a6442ab09e26faa99d0181f071cc60a1095baa3
Giosepe Luiz <giosepe_luiz3@live.com>
2021-08-04 16:11:27 -0300

    Redirecionamento inicial

--- next.config.js
+++ next.config.js
@@ -4,4 +4,18 @@ module.exports = {
     YT_KEY: process.env.YT_KEY,
     YT_ID: process.env.YT_ID,
   },
+  async redirects() {
+    return [
+      {
+        source: "epic.*/:id",
+        destination: "loja.*/epic/:id",
+        permanent: true,
+      },
+      {
+        source: "loja.*/:slug/:id",
+        destination: "/:slug/:id",
+        permanent: true,
+      },
+    ];
+  },
 };
