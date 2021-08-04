0b8002848225f1c9fc8ef5be603860ee645feb86
Giosepe Luiz <giosepe_luiz3@live.com>
2021-08-04 15:41:29 -0300

    Melhorias na função de responsividade

--- components/Main/ImageResponsive.jsx
+++ components/Main/ImageResponsive.jsx
@@ -1,34 +1,54 @@
 import Image from "next/image";
 import { useEffect, useState } from "react";
 
-const ImageResponsive = () => {
-  const [windowSize, setWindowSize] = useState({
-    width: null,
-  });
+// Verifica qual a dimensão da tela
+const getWindowDimensions = () => {
+  let width = 0;
+  if (typeof window !== "undefined") {
+    width = window.innerWidth;
+  }
+  return width;
+};
 
+// Componente que torna a imagem responsiva
+const ImageResponsive = () => {
+  const [windowWidth, setWindowWidth] = useState();
+
+  // Chama a função getWindowDimensions() que num primeiro momento não vai encontrar "window"
   useEffect(() => {
-    if (typeof window !== "undefined") {
-      window.addEventListener("load", () => {
-        setWindowSize({
-          width: window.innerWidth,
-        });
-      });
-      window.addEventListener("resize", () => {
-        setWindowSize({
-          width: window.innerWidth,
-        });
-      });
-    }
+    const width = getWindowDimensions();
+    setWindowWidth(width);
   }, []);
 
-  if (Number(windowSize.width) > 799) {
+  // Chama novamente a função getWindowDimensions() pra receber o valor de "width" da tela
+  // Toda vez que a tela for redimensionada, a função será chamada
+  useEffect(() => {
+    const handleResize = () => {
+      const width = getWindowDimensions();
+      setWindowWidth(width);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.addEventListener("resize", handleResize);
+  }, []);
+
+  // Retorna os componentes de imagem
+  if (windowWidth && windowWidth > 799) {
     return (
       <Image src="/images/logo-texto.svg" width={800} height={300} alt="Logo Fique no Controle" />
     );
   }
-  return (
-    <Image src="/images/logo-texto-alt.svg" width={650} height={300} alt="Logo Fique no Controle" />
-  );
+  if (windowWidth && windowWidth < 800) {
+    return (
+      <Image
+        src="/images/logo-texto-alt.svg"
+        width={650}
+        height={300}
+        alt="Logo Fique no Controle"
+      />
+    );
+  }
+  return null;
 };
 
 export default ImageResponsive;
