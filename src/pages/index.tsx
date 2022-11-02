9057cb5a41f2a83ce1eb733bc3b762f276d4e1af
Giosepe Luiz <giosepe_luiz3@live.com>
2022-11-02 23:04:44 +0000

    Atualização da página inicial.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,42 @@
 import Image from "next/image";
+import { useState } from "react";
 import styles from "../styles/Home.module.scss";
 
-const Home = () => (
-  <main className={styles["main-wrapper"]}>
-    <div className={styles["main-title"]}>
-      <span className={styles["main-title-emphasis"]}>&#10094;</span>
-      <span className={styles["main-title-middle"]}>ArqueiroVerde</span>
-      <span className={styles["main-title-emphasis"]}>/&#10095;</span>
-    </div>
+const Home = () => {
+  const [tipClass, setTipClass] = useState("none");
 
-    <div className={styles["main-subtitle"]}>
-      <span className={styles["main-subtitle-name"]}>Giosepe Luiz</span>
-      <span className={styles["main-subtitle-arrow"]}>{`=>`}</span>
-      <span className={styles["main-subtitle-dev"]}>{`{ desenvolvedor web }`}</span>
-    </div>
+  return (
+    <main className={styles["main-wrapper"]}>
+      <div className={styles["main-title"]}>
+        <span className={styles["main-title-emphasis"]}>&#10094;</span>
+        <span className={styles["main-title-middle"]}>ArqueiroVerde</span>
+        <span className={styles["main-title-emphasis"]}>/&#10095;</span>
+      </div>
 
-    <ul className={styles["main-menu"]}>
-      <li className={styles["main-menu-icon"]}>
-        <Image src="/images/logo-github-white.svg" width="70" height="70" alt="Logotipo Github" />
-      </li>
-    </ul>
-  </main>
-);
+      <div className={styles["main-subtitle"]}>
+        <span className={styles["main-subtitle-name"]}>Giosepe Luiz</span>
+        <span className={styles["main-subtitle-arrow"]}>{`=>`}</span>
+        <span className={styles["main-subtitle-dev"]}>{`{ desenvolvedor web }`}</span>
+      </div>
+
+      <ul className={styles["main-menu"]}>
+        <li className={styles["main-menu-icon"]}>
+          <a href="https://github.com/giosepeluiz" target="_blank" rel="noreferrer">
+            <Image
+              src="/images/logo-github-white.svg"
+              width="70"
+              height="70"
+              alt="Logotipo Github"
+              onMouseEnter={setTipClass("block")}
+              onMouseLeave={setTipClass("none")}
+            />
+          </a>
+        </li>
+      </ul>
+      <div className={styles["main-tip"]}>[descer]</div>
+      <div className={styles["main-arrow"]}>&#x21d3;</div>
+    </main>
+  );
+};
 
 export default Home;
